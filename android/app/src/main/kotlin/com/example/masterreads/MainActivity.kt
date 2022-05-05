f46ad303b6b021938900bfd7666bfe74459a4827
daarienz <rismi@graduate.utm.my>
2022-05-05 23:48:46 +0700

    Init

---
+++ android/app/src/main/kotlin/com/example/masterreads/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.masterreads
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color kAccentColor = Colors.purple.shade900;
-Color kPrimaryColor = Colors.white;
-Color kSecondaryColor = Colors.black;
-Color kAppBarBackground = Colors.white;
-Color kHighlightColor = Colors.red;
-Color kTextFieldColor = Color(0xFF979797);
