4bd49d4fd437045ebc68f57220c96f0e37a3f999
ellie-aluge <92854070+ellie-aluge@users.noreply.github.com>
2022-06-07 15:43:29 +0800

    Initiation of checkout backend using paypal

---
+++ lib/models/payment.dart
@@ -0,0 +1,16 @@
+class PaymentModel{
+  static const ID= "id";
+  static const PAYMENT_ID= 'paymentId';
+  static const Cart= 'cart';
+  static const Amount= 'amount';
+  static const Status= 'status';
+
+
+  String id='';
+  String paymentId='';
+  List cart=[];
+  String amount='';
+  String status='';
+
+
+}
